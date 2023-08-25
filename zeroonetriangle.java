bb8da4203a79623f34172b49969e1920cb67a905
Suchismita Banerjee <93653700+SuchismitaB03@users.noreply.github.com>
2023-08-25 21:52:54 +0530

    Update zeroonetriangle.java

--- zeroonetriangle.java
+++ zeroonetriangle.java
@@ -4,9 +4,9 @@ public class zeroonetriangle {
     public static void main(String[] args) {
         // Press Alt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
-        //System.out.printf("Hello and welcome!");
+        
         int n=5;
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
+        
         for (int i = 1; i <= n; i++) {
             for(int j=1;j<=i;j++){
               int sum =i+j;
@@ -19,9 +19,7 @@ public class zeroonetriangle {
             }
             System.out.println();
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            //System.out.println();
+            
         }
     }
 }
