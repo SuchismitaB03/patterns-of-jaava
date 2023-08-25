5c63c5ec3c3b400845e65727984f699b628d47eb
Suchismita Banerjee <93653700+SuchismitaB03@users.noreply.github.com>
2023-08-25 21:56:53 +0530

    Update invertedhalfpyramidsecond.java

--- invertedhalfpyramidsecond.java
+++ invertedhalfpyramidsecond.java
@@ -1,18 +1,10 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class invertedhalfpyramidsecond {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        //System.out.printf("Hello and welcome!");
         int n=4;
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
         for (int i = 1; i <= n; i++) {
          for(int j=1;j<=n-i;j++){
              System.out.print(" ");
          }
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
             for(int j=1;j<=i;j++){
                 System.out.print("*");
             }
