f02b8f0d7186736f857ddb39f7f10147ef377a6c
Suchismita Banerjee <93653700+SuchismitaB03@users.noreply.github.com>
2023-08-25 21:56:20 +0530

    Update numberpyramid.java

--- numberpyramid.java
+++ numberpyramid.java
@@ -1,12 +1,6 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class numberpyramid {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
        int n=5;
-
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
         //spaces
         for (int i = 1; i <= n; i++) {
          for(int j=1;j<=n-i;j++){
@@ -17,9 +11,6 @@ public class numberpyramid {
              System.out.print(i+" ");
          }
          System.out.println();
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-
-        }
+       }
     }
 }
