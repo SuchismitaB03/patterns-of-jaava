4ba4e88bdb3b1601a1e49a60eb7a9eb661017dae
Suchismita Banerjee <93653700+SuchismitaB03@users.noreply.github.com>
2023-08-25 21:55:41 +0530

    Update palindromic.java

--- palindromic.java
+++ palindromic.java
@@ -1,20 +1,12 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class palindromic {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        int n = 5;
-
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= n; i++) {
+       int n = 5;
+for (int i = 1; i <= n; i++) {
             //spaces
             for (int j = 1; j <= n - i; j++) {
                 System.out.print(" ");
             }
             //1st half
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
             for (int j = 1; j >= 1; j--) {
                 System.out.print(j);
             }
