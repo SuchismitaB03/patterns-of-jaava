61e9d1de93338e00878a1a3aff02b3ab5620de1a
Suchismita Banerjee <93653700+SuchismitaB03@users.noreply.github.com>
2023-08-25 21:55:01 +0530

    Update pyramidwithnumbers.java

--- pyramidwithnumbers.java
+++ pyramidwithnumbers.java
@@ -1,16 +1,11 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class pyramidwithnumbers {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        //System.out.printf("Hello and welcome!");
+       
         int n = 5;
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
+       
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= i; j++) {
-                // Press Shift+F9 to start debugging your code. We have set one breakpoint
-                // for you, but you can always add more by pressing Ctrl+F8.
+               
                 System.out.print(j + " ");
             }
             System.out.println();
